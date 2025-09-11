c17014a03a143b43908f8c500315198ae915f9af
admin <my@admin.com>
2025-09-11 20:17:31 +0800

    .

--- demo/app/Common.php
+++ demo/app/Common.php
@@ -2,15 +2,76 @@
 
 namespace demo\app;
 
-use AloneWebMan\Bot\BotHelper;
+use AlonePhp\Telegram\Bot;
+use AloneWebMan\Bot\BotReq;
 
-class Common extends BotHelper {
-    /**
-     * 执行main前公共执行
-     * @return bool 返回false不执行main
+class Common {
+    /*
+     * 当前插件名
      */
-    public function index(): bool {
-        return true;
+    public string $plugin = "";
+    /*
+     * 原样post
+     */
+    public array $post = [];
+    /*
+     * 当前主体信息
+     */
+    public array $data = [];
+    /*
+     * 请求token
+     */
+    public string $token = "";
+    /*
+     * 机器人key
+     */
+    public string $key = "";
+    /*
+     * 机器人信息表单
+     */
+    public mixed $bot = null;
+    /*
+     * 请求信息
+     */
+    public BotReq|null $req = null;
+    /*
+     * 当前 update_id
+     */
+    public string|int $update_id = '';
+    /*
+     * 发送信息 带回复id
+     */
+    public Bot|null $res = null;
+    /*
+     * 发送信息 alone_bot(key)
+     */
+    public Bot|null $tel = null;
+
+    /**
+     * @param string $plugin 插件名
+     * @param string $token  路由token
+     */
+    public function __construct(string $plugin, string $token) {
+        $this->plugin = $plugin;
+        $this->token = $token;
+    }
+
+    /**
+     * 处理信息入口
+     * @param mixed $req
+     * @return $this
+     */
+    public function handle(mixed $req): static {
+        $this->req = $req;
+        $this->post = $this->req->post ?? [];
+        $this->data = $this->req->data ?? [];
+        $this->update_id = $this->req->update_id ?? '';
+        $this->bot = $this->getBotInfo();
+        $this->key = $this->getBotKey();
+        $this->tel = alone_bot($this->key);
+        $this->res = $this->chat();
+        call_user_func([$this, 'main']);
+        return $this;
     }
 
     /**
@@ -27,5 +88,34 @@ class Common extends BotHelper {
      */
     public function getBotKey(): string {
         return ($this->bot['bot_ket'] ?? $this->getConfig('dev_bot_key'));
+    }
+
+    /**
+     * 获取配置
+     * @param string|null $key
+     * @param mixed       $default
+     * @return mixed
+     */
+    public function getConfig(string|null $key = null, mixed $default = null): mixed {
+        $config = alone_bot_config($this->plugin);
+        return isset($key) ? ($config[$key] ?? $default) : $config;
+    }
+
+    /**
+     * 聊天带id
+     * @return Bot
+     */
+    public function chat(): Bot {
+        $this->res = alone_bot($this->key)->chat_id($this->req->chat_id)->query_id($this->req->query_id)->message_id($this->req->msg_id);
+        return $this->res;
+    }
+
+    /**
+     * @param string|int $content
+     * @param bool       $alert
+     * @return Bot
+     */
+    public function callbackText(string|int $content, bool $alert = false): Bot {
+        return $this->tel->query_id($this->req->query_id)->callbackText($content, $alert);
     }
 }
