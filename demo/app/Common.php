7d32d464747c4d60acc541cdc17dd7fa0a70e228
admin <my@admin.com>
2025-09-11 19:43:43 +0800

    .

--- demo/app/Common.php
+++ demo/app/Common.php
@@ -6,6 +6,14 @@ use AloneWebMan\Bot\BotHelper;
 
 class Common extends BotHelper {
     /**
+     * 执行main前公共执行
+     * @return bool 返回false不执行main
+     */
+    public function index(): bool {
+        return true;
+    }
+
+    /**
      * 单个机器人信息
      * @return mixed
      */
@@ -19,13 +27,5 @@ class Common extends BotHelper {
      */
     public function getBotKey(): string {
         return ($this->bot['bot_ket'] ?? $this->getConfig('dev_bot_key'));
-    }
-
-    /**
-     * 执行main前公共执行
-     * @return bool 返回false不执行main
-     */
-    public function index(): bool {
-        return true;
     }
 }
